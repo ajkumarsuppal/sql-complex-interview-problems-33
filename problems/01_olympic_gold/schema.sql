57890e4efaea3bb7984e90b2769e58a1d378d2ef
Ajay Suppal <akrsuppal@gmail.com>
2026-01-03 07:44:40 +0530

    fix(01): explicitly qualify dbo schema and rerun seed

--- problems/01_olympic_gold/schema.sql
+++ problems/01_olympic_gold/schema.sql
@@ -1,3 +1,14 @@
 -- Purpose:
 -- Defines schema for Problem 01 (Olympic Gold Medals)
 -- Separated to allow CI, migrations, and reproducibility.
+drop table if exists events;
+
+CREATE TABLE dbo.events
+(
+    ID int,
+    event varchar(255),
+    YEAR int,
+    GOLD varchar(255),
+    SILVER varchar(255),
+    BRONZE varchar(255)
+);
