0420ed086c181223c7fbee7d0be1f681fabd84c7
Ajay Suppal <akrsuppal@gmail.com>
2026-01-03 07:29:02 +0530

    fix(01): explicitly qualify dbo schema and rerun seed

--- problems/01_olympic_gold/solution.sql
+++ problems/01_olympic_gold/solution.sql
@@ -1,2 +1,33 @@
 -- Primary solution.
 -- Chosen as default for readability and extensibility.
+
+--check server name
+SELECT @@SERVERNAME AS server_name;
+--check current user
+SELECT CURRENT_USER;
+--check current login
+SELECT SUSER_SNAME() AS login_name;
+--check schemas in the current database
+SELECT *
+FROM sys.schemas;
+--check current schema
+SELECT SCHEMA_NAME() AS current_schema;
+--find databases in the server
+select *
+from sys.databases;
+--check databases in the server
+SELECT name
+FROM sys.databases;
+
+--check current database
+SELECT DB_NAME() AS current_db;
+
+--use the olympics database
+use sql_problems;
+--check tables in the current database
+SELECT *
+FROM sys.tables;
+
+--view data in events table
+SELECT *
+FROM dbo.events;
